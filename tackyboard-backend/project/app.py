ca5b0aca08fe1772949b5a44ab0cec29b2fcda81
diana <diliang4783573@gmail.com>
2020-09-21 17:42:32 -0700

    basic webscrape functionalities for LinkedIn

--- tackyboard-backend/project/app.py
+++ tackyboard-backend/project/app.py
@@ -8,8 +8,9 @@ from flask_login import (
   logout_user
 )
 from models import db, connect_db, User
+from webscraper import scrapeAngelList
 
-# CURR_USER_KEY = "curr_user"
+CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
@@ -30,4 +31,32 @@ def add_user_to_g():
         g.user = None
 
 
+@app.route('/job-posts/new', methods=["POST"])
+def add_job_post():
+  """Add a job posting card. 
+  Request object expects: the job listing URL, origin OR form data for self-input
 
+  If the form isn't valid, return error. 
+
+  If a URL is provided, then run webscraper on the URL. The scraping steps will be based
+      on the origin_name provided.  
+  """
+
+  if request.json['job_url'] and request.json['origin']:
+    # execute webscrapper
+      # webscrapper returns the job details/company associated
+    # create an instance of the job_post model
+    # db.add(job_post_instance)
+    # db.commit()
+    # return successful json message or w/e
+  else {
+    #validate the form values with our form model
+      # its a form, so we can just create the job_post model instance
+      # db.add(job_post_instance)
+      # db.commit()
+  }
+
+  @app.route('/test', methods=["POST"])
+  def test():
+    scrapeAngelList(request.json['url'])
+    return ('Successful', 201)
