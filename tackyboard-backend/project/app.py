c60927946730e6a23c1c437bbbc93b9995cb6dfc
diana <diliang4783573@gmail.com>
2020-09-23 22:41:33 -0700

    added job post delete route, modified endpoints to be RESTful

--- tackyboard-backend/project/app.py
+++ tackyboard-backend/project/app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, request, redirect, session, g
 from flask_login import (
-  LoginManager,
-  current_user,
-  login_required,
-  login_user,
-  logout_user
+    LoginManager,
+    current_user,
+    login_required,
+    login_user,
+    logout_user,
 )
 from models import db, connect_db, User, JobPost, PostNote
 from flask_cors import CORS
@@ -15,12 +15,15 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///tackyboard')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dianaiscoolaf')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "postgresql:///tackyboard"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dianaiscoolaf")
 
 connect_db(app)
+
 
 @app.before_request
 def add_user_to_g():
@@ -32,28 +35,65 @@ def add_user_to_g():
         g.user = None
 
 
-@app.route('/job-posts/new', methods=["POST", "GET"])
+@app.route("/job-posts", methods=["POST"])
 def add_job_post():
-    """Add a job posting card. 
+    """Add a job posting card.
     Request object expects: the job listing URL, origin OR form data for self-input
 
-    If the form isn't valid, return error. 
+    If the form isn't valid, return error.
 
     If a URL is provided, then run webscraper on the URL. The scraping steps will be based
-        on the origin_name provided.  
+        on the origin_name provided.
+
+    If successful, return json object.
     """
 
     # if request.json['scrape'] = True, then run the webscraper. else, use manual input form.
-    
-        # execute webscrapper
-        # webscrapper returns the job details/company associated
-        # create an instance of the job_post model
-        # db.add(job_post_instance)
-        # db.commit()
-        # return successful json message or w/e
+
+    # execute webscrapper
+    # webscrapper returns the job details/company associated
+    # create an instance of the job_post model
+    # db.add(job_post_instance)
+    # db.commit()
+    # return successful json message or w/e
+
+    try:
+        post_url = request.json["post_url"]
+        company = request.json["company"]
+        position = request.json["position"]
+        origin_name = request.json["origin_name"]
+        job_description = request.json["job_description"]
+
+        new_job_post = JobPost.add(post_url, company, position, origin_name)
+
+        new_post_note = PostNote(
+            job_post_id=new_job_post.job_post_id,
+            note=job_description,
+            note_title="Job Listing Details",
+        )
+        db.session.add(new_post_note)
+        db.session.commit()
+
+    except Exception as e:
+        return (f"error occurred when creating the job post, {e}", 400)
+
+    return (
+        {
+            "job_post": new_job_post.serialize(),
+            "post_notes": [new_post_note.serialize()],
+        },
+        201,
+    )
 
 
+@app.route("/job-posts/<job_post_id>", methods=["DELETE"])
+def remove_job_post(job_post_id):
+    """
+    Removes a job post by job id.
+    Returns a json message confirmation of deletion success.
+    """
 
-# @app.route('/test', methods=["POST"])
-# def test():
-#     """Dev test route"""
+    JobPost.query.filter_by(job_post_id=job_post_id).delete()
+    db.session.commit()
+
+    return ({"message": f"Job Post #{job_post_id} has been deleted"}, 200)
--- tackyboard-backend/project/models.py
+++ tackyboard-backend/project/models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(60), nullable=False, unique=True)
@@ -54,7 +54,7 @@ class User(db.Model):
 
 class ApplicationStatus(db.Model):
 
-    __tablename__ = 'application_statuses'
+    __tablename__ = "application_statuses"
 
     status_id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String, nullable=False)
@@ -62,50 +62,104 @@ class ApplicationStatus(db.Model):
 
 class ClickCopyNote(db.Model):
 
-    __tablename__ = 'click_copy_notes'
+    __tablename__ = "click_copy_notes"
 
     clickcopy_note_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False
+    )
     note = db.Column(db.Text, nullable=False)
 
-    user = db.relationship('User', backref='click_copy_notes')
+    user = db.relationship("User", backref="click_copy_notes")
 
-# class UrlOrigin():
-
-#     __tablename__ = 'url_origins'
-
-#     url_origin_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     origin_name = db.Column(db.String(50), nullable=False, default="Unknown")
 
 class JobPost(db.Model):
 
-    __tablename__ = 'job_posts'
+    __tablename__ = "job_posts"
 
     job_post_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     post_url = db.Column(db.String, nullable=False)
     company = db.Column(db.String(50), nullable=False)
-    application_status_id = db.Column(db.Integer, db.ForeignKey('application_statuses.status_id'), nullable=False, default=0)
-    position = db.Column(db.String(50), nullable=False, default='Unknown')
+    application_status_id = db.Column(
+        db.Integer,
+        db.ForeignKey("application_statuses.status_id"),
+        nullable=False,
+        default=0,
+    )
+    position = db.Column(db.String(50), nullable=False, default="Unknown")
     origin_name = db.Column(db.String(50), nullable=False, default="Unknown")
     created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
-    last_status_update = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    last_status_update = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow()
+    )
     interview_date_time = db.Column(db.DateTime, nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=True
+    )
 
-    user = db.relationship('User', backref='job_posts')
-    # origin = db.relationship('UrlOrigin', backref='job_posts')
-    status = db.relationship('ApplicationStatus', backref='job_posts')
+    user = db.relationship("User", backref="job_posts")
+    status = db.relationship("ApplicationStatus", backref="job_posts")
+
+    def serialize(self):
+        """Serialize the instance object to be sent as JSON."""
+        return {
+            "job_post_id": self.job_post_id,
+            "post_url": self.post_url,
+            "company": self.company,
+            "application_status_id": self.application_status_id,
+            "position": self.position,
+            "origin_name": self.origin_name,
+            "created_date": self.created_date,
+            "last_status_update": self.last_status_update,
+            "interview_date_time": self.interview_date_time,
+            "user_id": self.user_id,
+        }
+
+    @classmethod
+    def add(cls, post_url, company, position, origin_name):
+        """
+        Creates an instance of JobPost and inserts into the database.
+        Returns a job_post instance.
+        """
+
+        new_job_post = JobPost(
+            post_url=post_url,
+            company=company,
+            position=position,
+            origin_name=origin_name,
+        )
+
+        try:
+            db.session.add(new_job_post)
+            db.session.flush()  # use flush() to access the unique primary key `job_post_id`
+        except Exception as e:
+            return {"error": {"Database add error": e}}
+
+        return new_job_post
 
 class PostNote(db.Model):
 
-    __tablename__ = 'post_notes'
+    __tablename__ = "post_notes"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    post_id = db.Column(db.Integer, db.ForeignKey('job_posts.job_post_id', ondelete='CASCADE'), nullable=False)
+    job_post_id = db.Column(
+        db.Integer,
+        db.ForeignKey("job_posts.job_post_id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    note_title = db.Column(db.Text, nullable=False)
     note = db.Column(db.Text, nullable=False)
 
-    job_post = db.relationship('JobPost', backref='post_notes')
+    job_post = db.relationship("JobPost", backref="post_notes")
 
+    def serialize(self):
+        """Serialize the instance object to be sent as JSON."""
+        return {
+            "post_note_id": self.id,
+            "job_post_id": self.job_post_id,
+            "note_title": self.note_title,
+            "note": self.note
+        }
 
 def connect_db(app):
     """Connect this database to provided Flask app."""
